d2c84aceafef3bacaf5c867a435190540c896af9
Mandell1981 <mandell316@gmail.com>
2024-04-30 10:54:25 -0400

    homework

---
+++ function_pratice.py
@@ -0,0 +1,28 @@
+def hello():
+    print("Hello, How are you Today!")
+    
+
+
+def pack(num1, num2, num3):
+    return [num1, num2, num3]
+
+result = pack(3, 1, 6)
+print(result)
+
+
+
+def eat_lunch(food_list):
+    if not food_list:
+        print("My belly is empty!")
+    else:
+        print("First I eat", food_list[0])
+        for food_list in food_list[1:]:
+            print("Next I eat", food_list[1])
+
+
+
+hello()
+eat_lunch(["salad", "smoothie", "fruit"])
+
+
+           
